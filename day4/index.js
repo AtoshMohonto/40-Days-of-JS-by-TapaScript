4fc07d99f93206cd9d85897c34e332912292b0ff
Atosh <atoshmohonto11@gmail.com>
2025-06-16 13:51:05 +0600

    day 4 cls complt in dokan 1 50 pm

--- day4/index.js
+++ day4/index.js
@@ -6,6 +6,10 @@ if (condition) {
    // code to be executed if condition is false 
 }
 */
+//tuirnary operator
+let cBuz=false;
+cBuz ? console.log("I will"):console.log("i won't")
+
 //#1
 let catchingBus=true;
 if (catchingBus) {
@@ -40,18 +44,18 @@ else if(score<40){
 }
 
 //4 :independent if 
-let x = 0;
-if(x===0){
+let a = 0;
+if(a===0){
     console.log(0)
 }
-if(x>=0){
+if(a>=0){
     console.log("GRETER THAN ZERO")
 }
-if(x<=0){
+if(a<=0){
     console.log("LESS THAN ZERO")
 }
 
-//5 : EARLY EXIT ,RECOMENDERD
+//5 : EARLY EXIT ,RECOMENDERD      if else
 let x = 0;
 if(x===0){
     console.log(0)
@@ -63,4 +67,104 @@ else if(x<=0){
     console.log("LESS THAN ZERO")
 }
 
-// 6 
+// 6 nested if else
+
+if (true){
+    console.log("6outer if")
+    if(true){
+        console.log("6inner if")
+    }else{
+        console.log("6inner else")
+    }
+}else{
+    console.log("6outer else")
+}
+
+// 7 nested if else with variales
+const condition = false
+const innercondition = true
+if (condition){
+    console.log("7outer if")
+    if(innercondition){
+        console.log("7inner if")
+    }else{
+        console.log("7inner else")
+    }
+}else{
+    console.log("7outer else")
+}
+
+///switch and case 27min
+//multiple condition babsed on single value 
+
+let position =10;
+switch(position){
+    case 1:
+        console.log("print 1");
+    break;
+    case 2:
+        console.log("print 2");
+    break;
+    case 3:
+        console.log("print 3");
+    break;
+    case 4:
+        console.log("print 4");
+    break;
+    default:
+        console.log("nothing matched")
+}
+
+let day= 14;
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+}
+//switch jumps ,for too much condition  use switch 
+let name = "John";//""
+switch(name){
+    case "John":
+        console.log("Hello John");
+        break;
+    case "Jane":
+        console.log("Hello Jane");
+        break;
+    case "Doe":
+        console.log("Hello Doe");
+        break;
+    default:
+        console.log("Hello Guest");
+}
+
+const city = "nilphamari";
+switch(city) {
+    case "nilphamari":
+    case "dhaka":
+    case "rj":
+    console.log("bangladesh");
+    break;
+    case "delli":
+        default:
+            console.log("india");
+}
