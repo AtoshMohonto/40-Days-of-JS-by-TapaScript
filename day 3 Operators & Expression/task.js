662762b1b8dedfc40f4ae85eb95f8303d24404e5
Atosh <atoshmohonto11@gmail.com>
2025-06-19 07:12:26 +0600

    task

--- day 3 Operators & Expression/task.js
+++ day 3 Operators & Expression/task.js
@@ -1,1 +1,6 @@
-let a =5;
+//task 1
+let a;
+if (a%2==0){
+    console.log("Even");
+}else
+    console.log("odd");
---
+++ day4 IF ELSE SWITCH/task.js
@@ -0,0 +1,18 @@
+//task 01
+ let day = "Monday";
+switch (day) {
+    case "monday":
+        console.log("It's the start of the week.");
+        break;
+    default:
+        console.log("It's a normal day."); 
+        //eta output hobe kkaron day er value "Monday" 
+}
+///task 02
+let amount =100;
+if (amount==100){
+    console.log("Withdrawal successful");
+}else
+console.log("Invalid amount");
+
+//task 03
